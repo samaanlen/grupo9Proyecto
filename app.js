dae333df0f5c1761998d7922f26a0af2ff6c402c
Natalia <npareti@hotmail.com>
2022-01-20 20:42:11 -0300

    Login, Register

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ const app = express();
 
 app.use( express.static("public") ) 
 
-app.get ("/home", (req, res) => {
+app.get ('/', (req, res) => {
     res.sendFile(__dirname + "/views/home.html")
 })
 
-
-
 app.listen( 3000, () =>{ 
-    console.log ("servidor coerriendo en puerto 3000")
+    console.log ("servidor corriendo en puerto 3000")
 })
 
 // app.get('/', (req,res)=>{
