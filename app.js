45f08c24960675f55014dde9bc5c4faa276a431b
¨Lenin <lenin.samaan@gmail.com>
2022-01-20 10:29:32 -0400

    actualizacion de la estructura del proyecto

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log ("Inicio de Proycto")
+const express = require("express");
+
+const app = express();
+
+app.use( express.static("public") ) 
+
+app.get ("/home", (req, res) => {
+    res.sendFile(__dirname + "/views/home.html")
+})
+
+
+
+app.listen(process.env.PORT || 3000, () =>{ 
+    console.log ("servidor coerriendo en puerto 3000")
+})
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
