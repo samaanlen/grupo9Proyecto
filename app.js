d9ea7503983291a6b173cbea5c7b823813ea4d6b
Natalia <npareti@hotmail.com>
2022-01-20 23:08:17 -0300

    modificacion app

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use( express.static("public") ) 
 
-app.get ('/', (req, res) => {
+app.get ('/home', (req, res) => {
     res.sendFile(__dirname + "/views/home.html")
 })
 
@@ -16,10 +16,10 @@ app.listen( 3000, () =>{
 //    res.sendFile(__dirname + '/views/home.html');
 //});
 
-// app.get('/login', (req,res)=>{
-//    res.sendFile(__dirname + '/views/login.html');
-// });
-
-// app.get('/register', (req,res)=>{
-//    res.sendFile(__dirname + '/views/register.html');
-// });
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+ });
+ 
+ app.get('/register', (req,res)=>{
+   res.sendFile(__dirname + '/views/register.html');
+ });
