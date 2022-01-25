d23a0bf4cf7d151306c8d38f6a77a4e94919dc1b
¨Lenin <lenin.samaan@gmail.com>
2022-01-25 04:37:28 -0400

    nuevas versiones del home y la hoja de estilo

--- app.js
+++ app.js
@@ -4,22 +4,24 @@ const app = express();
 
 app.use( express.static("public") ) 
 
-app.get ('/home', (req, res) => {
+app.get ("/home", (req, res) => {
     res.sendFile(__dirname + "/views/home.html")
 })
 
-app.listen( 3000, () =>{ 
+
+
+app.listen(process.env.PORT || 3000, () =>{ 
     console.log ("servidor corriendo en puerto 3000")
 })
 
-// app.get('/', (req,res)=>{
-//    res.sendFile(__dirname + '/views/home.html');
-//});
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
 
 app.get('/login', (req,res)=>{
     res.sendFile(__dirname + '/views/login.html');
- });
- 
- app.get('/register', (req,res)=>{
-   res.sendFile(__dirname + '/views/register.html');
- });
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
